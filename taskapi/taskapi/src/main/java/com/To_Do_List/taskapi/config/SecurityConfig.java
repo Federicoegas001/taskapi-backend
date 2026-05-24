0791b4d6e2171c6f7f2450b42c853013249fa917
Federicoegas001 <egasagustin@gmail.com>
2026-05-24 13:13:11 -0300

    cambio: adminitiendo todas las rutas de vercel

--- taskapi/taskapi/src/main/java/com/To_Do_List/taskapi/config/SecurityConfig.java
+++ taskapi/taskapi/src/main/java/com/To_Do_List/taskapi/config/SecurityConfig.java
@@ -36,9 +36,10 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of(
+        config.setAllowedOriginPatterns(List.of(
                 "http://localhost:5173",
-                "https://taskapi-frontend.vercel.app"
+                "https://taskapi-frontend.vercel.app",
+                "https://*.vercel.app"
         ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
