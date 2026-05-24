4754c6d583325e2f46762662a4eef980bc686f46
Federicoegas001 <egasagustin@gmail.com>
2026-05-24 00:02:42 -0300

    config: cambio authorizeHttpRequests

--- taskapi/taskapi/src/main/java/com/To_Do_List/taskapi/config/SecurityConfig.java
+++ taskapi/taskapi/src/main/java/com/To_Do_List/taskapi/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import com.To_Do_List.taskapi.security.JwtAuthFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -53,6 +54,7 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .requestMatchers("/auth/**").permitAll()
                         .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
                         .anyRequest().hasAnyRole("USER", "ADMIN")
