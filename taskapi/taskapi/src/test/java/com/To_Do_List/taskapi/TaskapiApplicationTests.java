8530c01d25273bc4c28ea8c39e670b31f73b5b69
Federicoegas001 <egasagustin@gmail.com>
2026-05-23 22:59:44 -0300

    config: variables de entorno para Railway

---
+++ taskapi/taskapi/src/test/java/com/To_Do_List/taskapi/TaskapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.To_Do_List.taskapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
